10c1248ccce0e8bb90878780439d1ef73e8efcfd
Siddhi Lokhande <siddhilokhande2202@gmail.com>
2025-03-29 20:05:37 +0530

    LOOPS

---
+++ 08_LOOPS/02_WHILE LOOP/01_MULTIPLICATION_TABLE.py
@@ -0,0 +1,6 @@
+n = 5  # Change this number to generate a different multiplication table
+a = 1
+
+while a <= 10:  # Colon (:) is required at the end of while statement
+    print(a, "x", n, "=", a * n)  # Remove the extra colon (:)
+    a += 1  # Indent this line correctly
---
+++ 08_LOOPS/04_NESTED LOOP/01_PATTERN.py
@@ -0,0 +1,4 @@
+ for i in range(1, 7):  # No extra spaces before 'for'
+    for j in range(1, i + 1):  # Correct indentation
+        print("*", end=" ")  # Correct indentation
+    print()  # Ensures a new line after each row
