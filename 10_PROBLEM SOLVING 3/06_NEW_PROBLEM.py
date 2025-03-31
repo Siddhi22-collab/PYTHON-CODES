114522a29e1b6ef28ab323e830b89400a81bd4bf
Siddhi Lokhande <siddhilokhande2202@gmail.com>
2025-03-31 20:00:05 +0530

    UPDATE'

---
+++ 10_PROBLEM SOLVING 3/06_NEW_PROBLEM.py
@@ -0,0 +1,31 @@
+#A=" why fit in, when you are Born to stand out!"
+#1. WRITE A PROGRAM TO FIND THE LENGTH OF THE FOLLOWING STRING
+A =  "why fit in, when you are Born to stand out!" # Ensure A is assigned some value
+print(len(A))
+
+
+#2. WRITE A PROGRAM TO CHECK HOW MANY TIMES ALPHABET O IS OCCURING
+A =  "why fit in, when you are Born to stand out!"
+print(A.count("o"))
+
+
+#3. WRITE A PROGRAM TO CONVERT THE WHOLE STRING INTO LOWER AND UPPER CASE
+#LOWER CASE
+A =  "why fit in, when you are Born to stand out!"
+x=A.lower()
+print(x)
+#UPPER CASE
+A =  "why fit in, when you are Born to stand out!"
+y=A.upper
+print(y)
+
+
+#4. WRITE A PROGRAM TO CONVERT THE FOLLOWING STRING INTO THE TITLE
+A =  "why fit in, when you are Born to stand out!"
+Q=A.lower()
+print(Q)
+
+
+#5. WRITE A PROGRAM TO FIND THE INDEX NUMBER OF "FIT IN"
+A =  "why fit in, when you are Born to stand out!"
+print(A.find("fit in"))
