9749c694f00f93343b38d4c8674c7537bb37ca6b
Siddhi Lokhande <siddhilokhande2202@gmail.com>
2025-04-01 19:35:23 +0530

    STRING FUNCTION 1

---
+++ 12_STRING MANIPULATION/01_STRING FUNCTION 1/01_ALL_FUNCTION.py
@@ -0,0 +1,23 @@
+a="my name is siddhi lokhande"
+print(a)
+
+#length
+print(len(a))
+
+#count
+print(a.count("i"))
+
+#uppercase
+print(a.upper())
+
+#lowercase
+print(a.lower())
+
+#index
+print(a.index("i"))
+
+#capitalize
+print(a.capitalize())
+
+#casefold
+print(a.casefold())
